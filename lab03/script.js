91112254c444a6705e68e2fa8587ca96e9d7b225
jyc02 <jchin2250@gmail.com>
2023-06-25 16:25:03 -0500

    calculator

---
+++ lab03/script.js
@@ -0,0 +1,20 @@
+function calc(value){
+    if (value == 'pow'){
+        document.getElementById("display").value = document.getElementById("display").value * document.getElementById("display").value;
+    } else if (value == 'sqrt'){
+        document.getElementById("display").value = Math.sqrt(document.getElementById("display").value);
+    } else if (value == 'sin'){
+        document.getElementById("display").value = Math.sin(document.getElementById("display").value);
+    } else if (value == 'cos'){
+        document.getElementById("display").value = Math.cos(document.getElementById("display").value);
+    } else if (value == 'tan'){
+        document.getElementById("display").value = Math.tan(document.getElementById("display").value);
+        console.log("tan passed");
+    } else if (value == '='){
+        document.getElementById("display").value = eval(document.getElementById("display").value);
+    }else if(value == ''){
+        document.getElementById("display").value = value;
+    } else {
+        document.getElementById("display").value += value;
+    }
+}
